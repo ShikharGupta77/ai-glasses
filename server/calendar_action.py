ec0bd75250c71c8679513a25032aa80d258862ff
Shikhar Gupta <shikharscout@gmail.com>
2023-10-29 00:21:16 -0700

    calendar stuff

--- server/calendar_action.py
+++ server/calendar_action.py
@@ -3,7 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 
 # date is YYYY/MM/DD
-def calendar_add(date, startTime, endTime):
+def calendar_add(date, startTime, endTime, title):
     with SB(uc=True, headed=True) as driver:
         driver.get("https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&hl=en&ifkv=AVQVeyynx9vTWrORetNTDJstyP9KRREywFTztGbPzXPZOA0CLrrBLZexaIuorYLmTqCW3xH2qcyPmg&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-653967197%3A1698546704446367&theme=glif")
         driver.type("#identifierId", 'calhacks224')
@@ -14,13 +14,28 @@ def calendar_add(date, startTime, endTime):
         driver.get(f'https://calendar.google.com/calendar/u/0/r/day/{date}')
         element = driver.find_element(By.CSS_SELECTOR, ".WJVfWe.A3o4Oe")
         element.click()
-        startTime = driver.find_element(By.XPATH, '//*[@data-key="startTime"]')
-        endTime = driver.find_element(By.XPATH, '//*[@data-key="endTime"]')
-        driver.execute_script("arguments[0].textContent = arguments[1];", startTime, "5:00pm")
-        driver.execute_script("arguments[0].textContent = arguments[1];", endTime, "6:00pm")
-        time.sleep(10)
+        startTimeElement = driver.find_element(By.XPATH, '//*[@data-key="startTime"]')
+        endTimeElement = driver.find_element(By.XPATH, '//*[@data-key="endTime"]')
+        time.sleep(2)
+        driver.execute_script("arguments[0].textContent = arguments[1];", startTimeElement, startTime)
+        driver.execute_script("arguments[0].textContent = arguments[1];", endTimeElement, endTime)
+        input_element = driver.find_element(By.CSS_SELECTOR, ".VfPpkd-fmcmS-wGMbrd")
+        #titleElement.click()
+        #time.sleep(2)
+        #driver.execute_script("arguments[0].setAttribute('value', arguments[1]);", titleElement, title)
+        #driver.execute_script("arguments[0].textContent = arguments[1];", titleElement, "Your New Text")
+        #input_element = driver.find_element(By.CSS_SELECTOR, '#c4305')
+        #driver.execute_script("arguments[0].setAttribute('placeholder', 'New Placeholder')", input_element)
+        #driver.execute_script("arguments[0].setAttribute('aria-label', 'New Aria Label')", input_element)
+        # Assuming you've found the input_element as shown above
+        #input_element.clear()  # Clear existing value/text
+        #input_element.send_keys('New Text')  # This simulates typing into the input field
+        time.sleep(2)
+        saveButton = driver.find_element(By.CSS_SELECTOR, ".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.pEVtpe")
+        saveButton.click()
+        time.sleep(15)
        
        
 
 if __name__ == '__main__':
-    calendar_add(date='2023/10/29', startTime='5:00pm', endTime='6:00pm')
+    calendar_add(date='2023/10/29', startTime='7:00pm', endTime='8:00pm', title="Meeting with Kushi")
